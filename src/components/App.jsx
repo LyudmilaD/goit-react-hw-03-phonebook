ee89fb8ea5fa0ca9a6e9dfd2ee471dadfc99f6d3
Lyudmila Dermenzhii <lsmolyaninova@gmail.com>
2022-05-29 17:34:01 +0300

    edit phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ export class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    filter: 'aaa',
+    filter: '',
   };
   submitHandler = obj => {
     this.setState(prevState => ({
